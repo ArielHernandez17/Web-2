f804bbfc083bf05518503839f4d6e3b1d2dac772
ArielHernandez17 <arielvelazquez89@aragon.unam.mx>
2026-02-25 10:06:07 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+//Concatenar datos
+var dato1= 'hola';
+var dato2= 'mundo';
+var dato3= ' '
+
+var resultado = dato1+dato3+dato2;
+console.log(resultado); 
---
+++ app3.js
@@ -0,0 +1,7 @@
+//Comparar
+var resultado=20 <10;
+
+console.log(resultado);
+
+var resultado=8<10;
+console.log(resultado);
