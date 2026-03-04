71d6869be95b9ba429fbea2ccfc621b851716a3c
Ariel <harielv17@gmail.com>
2026-03-04 10:07:12 -0600

    Fundamentos 3

---
+++ fundamentos3.js
@@ -0,0 +1,15 @@
+var estado=true;
+
+if(estado){
+    var estado = false;
+}
+console.log(estado);
+
+console.log("---------------------");
+
+let Estado=true;
+
+if (Estado){
+    let Estado=false;
+}
+console.log(Estado);
