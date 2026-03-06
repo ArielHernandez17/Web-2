f23c3838e71a1c8371b9f21fdd58f8b9fef9aac7
Ariel <harielv17@gmail.com>
2026-03-06 13:09:51 -0600

    borrado

--- app7.js
+++
@@ -1,6 +0,0 @@
-//2/3/26 while
-let dato = 1;
-while (dato <=10){
-    console.log("Numero: "+dato);
-    dato++;
-}
--- app9.js
+++
@@ -1,7 +0,0 @@
-//clase 2/3/26
-//generar numeros aleatorios
-function NumeroAletorio (min,max){
-    return Math.floor(Math.random()*(max,min))+min;
-
-}
-console.log (NumeroAletorio(1-10))
