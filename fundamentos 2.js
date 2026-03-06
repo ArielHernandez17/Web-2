733d26e9c15a9e4ae56683773cc907ff12e55696
Ariel <harielv17@gmail.com>
2026-03-06 13:14:29 -0600

    Eliminado

--- fundamentos 2.js
+++
@@ -1,9 +0,0 @@
-//operador terniario
-let nombre="ariel";
-let estado = false;
-
-console.log( `
-    ${estado ?'Genial ingresaste :D ' : 'No ingresaste :( '}${nombre}
-    `);
-console.log (`Hola ${nombre.toUpperCase()}`);
-
