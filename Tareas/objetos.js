52f814e3e88cd3eb59aae162e33c8908abb13357
Ariel <Harielv17@gmail.com>
2026-03-18 11:46:36 -0600

    Se subio la tarea

--- Tareas/objetos.js
+++ Tareas/objetos.js
@@ -1,20 +1,3 @@
-/*
-var miCarro = new Object();
-miCarro.marca='ford';
-miCarro.modelo='fiesta';
-miCarro.anio=2017;
-
-console.log(miCarro);
-
-var miCarro={
-    marca:'ford',
-    modelo:'fiesta',
-    anio: 2018
-}
-
-console.log (miCarro);
-*/
-
 const alumno ={
     id:4541251452,
     nombre:'Ariel',
@@ -53,35 +36,9 @@ const alumno ={
 
         }
     },
-    /*
-    actividadesPersonales: {
-        Juego:'The last of us part 2',
-        fisica:'correr',
-        actividadCotidiana (actividad){
-            return `${this.fisica} en el horario de ${actividad}` ;
-        }
-    }
-        */ 
+   
 };
 
-//console.log (alumno.actividadesPersonales.actividadCotidiana("7am"));
 console.log (alumno.educacion.primaria.hacerPipi());
 console.log (alumno.educacion.primaria.jugar(" Pelota"));
 console.log (alumno.educacion.secundaria.serRebeldes("platicar en el salon"))
-
-
-console.log(alumno.hasOwnProperty("numeroCuenta"));
-console.log(alumno.hasOwnProperty("Nombre"))
-
-console.log(alumno);
-//crear
-alumno.numeroCuenta=56268625;
-//leer
-console.log(alumno[ 'numeroCuenta']);
-//actualizar
-alumno.nombre="Aron";
-console.log (alumno[ 'nombre' ]);
-//eliminar
-delete alumno.segundoApellido;
-
-console.log(alumno);
