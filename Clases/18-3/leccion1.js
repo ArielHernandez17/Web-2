af060a146623d00c5e66bc91db5b5e5ab9ba43de
Ariel <Harielv17@gmail.com>
2026-03-18 03:34:21 -0600

    Clase del 18-3

---
+++ Clases/18-3/leccion1.js
@@ -0,0 +1,11 @@
+let frutas = ['manzana','peras'];
+
+/*
+frutas.forEach((fruta,index,array)=>{
+    console.log(fruta);
+    console.log(index);
+    console.log(array);
+})
+    */
+
+frutas.forEach(fruta=>console.log(fruta));
---
+++ Clases/18-3/leccion2.js
@@ -0,0 +1,6 @@
+var miCarro = new Object();
+miCarro.marca='ford';
+miCarro.modelo='fiesta';
+miCarro.anio=2017;
+
+console.log(miCarro);
